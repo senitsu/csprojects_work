ca2fa861e7d2055ce97d489f3b1d90a72ec958d4
senitsu <89802983+senitsu@users.noreply.github.com>
2021-09-08 14:29:15 -0700

    changed code

--- hello_p5/hello_p5.js
+++ hello_p5/hello_p5.js
@@ -1,16 +1,23 @@
 function setup() {
   createCanvas(400, 400);
+  background(0);
+
+  for(let i=0; i<10; i++)
+  {
+    fill(random(256),random(256),random(256));
+    let x = random(0, width);
+    let y = random(0, height);
+    rect(x,y,50,20);
+  }
 }
 
 let x = 50;
 
 function draw() {
-  background(0);
 
-  fill(255,0,0);
-  ellipse(x, 200, 100, 50);
-  x++;
+}
 
-  if (x > width+50)
-    x = -50;
+function mousePressed(){
+  fill(random(256), random(256), random(256));
+  ellipse(mouseX, mouseY, 20, 20);
 }
