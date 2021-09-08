cec6d72ddc1de4b0065b9f12ff2a0cad91906e2e
senitsu <89802983+senitsu@users.noreply.github.com>
2021-09-08 14:10:38 -0700

    added hello_p5 folder

---
+++ hello_p5/hello_p5.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+let x = 50;
+
+function draw() {
+  background(0);
+
+  fill(255,0,0);
+  ellipse(x, 200, 100, 50);
+  x++;
+
+  if (x > width+50)
+    x = -50;
+}
